1c6f9fdfdecb270d718290200982f5c47ff4a64e
Daniel Ignatov <sa3ana3a@abv.bg>
2017-03-14 17:26:50 +0200

    Added Entity Models.

---
+++ SharpForum.Models/EntityModels/Category.cs
@@ -0,0 +1,26 @@
+﻿namespace SharpForum.Models.EntityModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        #region Constructors
+        public Category()
+        {
+            this.SubCategories = new HashSet<SubCategory>();
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<SubCategory> SubCategories { get; set; }
+        #endregion
+    }
+}
---
+++ SharpForum.Models/EntityModels/SubCategory.cs
@@ -0,0 +1,26 @@
+﻿namespace SharpForum.Models.EntityModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class SubCategory
+    {
+        #region Constructors
+        public SubCategory()
+        {
+            this.Topics = new HashSet<Topic>();
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Topic> Topics { get; set; }
+        #endregion
+    }
+}
